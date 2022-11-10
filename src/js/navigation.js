8d89f5096dc4b8e04ab212ab9991cfc916d3aed6
Liam Atkins <liam.atkins@investisdigital.com>
2022-11-10 10:52:26 +0000

    Additional styles

--- src/js/navigation.js
+++ src/js/navigation.js
@@ -1,4 +1,14 @@
-function myFunction() {
-  var element = document.getElementById("myDIV");
-  element.classList.toggle("mystyle-active");
-}
+(function ($, window, Drupal) {
+  Drupal.behaviors.exampleTheme = {
+    attach: function (context, settings) {
+      $( "#burgerMenu", context ).click(function() {
+        $('#myDIV').toggleClass("mystyle-active");
+      });
+
+      $( "#burgerMenuClose", context ).click(function() {
+        $('#myDIV').removeClass("mystyle-active");
+      });
+    }
+  }
+
+})(jQuery, window, Drupal);
